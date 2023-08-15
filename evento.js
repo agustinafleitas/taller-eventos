c196fe27d92746c47499c052b7f9a66b5fafb1b2
agustinafleitas <agustinaafd@gmail.com>
2023-08-14 23:00:20 -0300

    Arreglo boton y div

--- evento.js
+++ evento.js
@@ -1,6 +1,13 @@
-function DivSaluda(){
+function DivSaluda(event){
     alert("Hola! Soy el div");
+    event.stopPropagation();
 }
 
 const areaDiv = document.getElementById("boton");
 areaDiv.addEventListener("click", DivSaluda);
+
+
+function saludar(event) {
+    alert("Hola!");
+    event.stopPropagation();
+}
